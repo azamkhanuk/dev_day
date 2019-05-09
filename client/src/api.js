e31ba9b5c7b21cd1b3debb7dc7dd2c47faf1c1a7
Umar--Khan <umarkhandev@gmail.com>
2019-05-09 09:56:11 +0100

    crud_on_page

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,41 @@
 let userId = 1
 
 const userURL = `http://localhost:3000/users/${userId}`
+const taskURL = `http://localhost:3000/tasks/`
 
 const getUser = () => 
     fetch(userURL)
     .then(resp => resp.json())
 
+const createTask = task =>
+    fetch(taskURL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: task.name,
+            user_id: task.user_id,
+            content: task.content,
+            deadline: task.deadline,
+            status: task.status,
+            image_url: task.image_url,
+            tag: task.tag
+        })
+    }).then(resp => resp.json())
+
+const deleteTask = task =>
+    fetch(taskURL + `${task.id}`, {
+        method: 'DELETE'
+    }).then(resp => resp.json())
+
+const updateTask = task => 
+fetch(taskURL + `${task.id}`, {
+    method: 'PATCH',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    }).then(resp => resp.json())
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -16,15 +16,15 @@ Task.destroy_all
 Comment.destroy_all
 
 
-User.create(name:'Scott', title: 'TCA Lead', location: 'R* North', assigner_id: 1, image_url: 'xD')
+User.create(name:'Scott', title: 'TCA Lead', location: 'R* North', assigner_id: 1, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg')
 
 Task.create(user_id: 1, name: 'Fix lod - NB_01', content: 'Sam level lod plz', 
-            deadline: Time.now, status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Americas, A class')
+            deadline: Time.at(Time.now.to_i + 46200), status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'High')
 
 Comment.create(task_id: 1, content: 'fix this shit yo')
 
 Task.create(user_id: 1, name: 'new lods', content: 'remake all the lods in the game', 
-    deadline: Time.now, status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Americas, A class')
+    deadline: Time.at(Time.now.to_i + 70000), status: false, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Medium')
 
 Comment.create(task_id: 2, content: 'adadad')
 Comment.create(task_id: 2, content: 'fix this saadad')
@@ -32,7 +32,7 @@ Comment.create(task_id: 2, content: 'fix adad')
 
 
 Task.create(user_id: 1, name: 'new lods for church', content: 'remake SD', 
-    deadline: Time.now, status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Americas, A class')
+    deadline: Time.at(Time.now.to_i + 55400), status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Low')
 
 Comment.create(task_id: 3, content: 'adaadadd')
 Comment.create(task_id: 3, content: 'fixadads saadad')
@@ -40,3 +40,12 @@ Comment.create(task_id: 3, content: 'fix adad')
 Comment.create(task_id: 3, content: 'adadad')
 Comment.create(task_id: 3, content: 'fix adadhis saadad')
 Comment.create(task_id: 3, content: 'fix adadad')
+
+User.create(name:'Tim', title: 'Junior TCA', location: 'R* Leeds', assigner_id: 2, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg')
+
+Task.create(user_id: 2, name: 'Bank needs more dirt', content: 'Aaron lod plz', 
+    deadline: Time.at(Time.now.to_i + 66200), status: true, image_url: 'https://png.pngtree.com/svg/20170505/b6219c949e.svg', tag: 'Jack, D class')
+
+Comment.create(task_id: 4, content: 'Test needs testing')
+Comment.create(task_id: 1, content: 'Beast Mode')
+
